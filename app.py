824de7d33b44017e2ca1e038531522e3fe9fd9dd
Kamal Ennaji <najikamal100@gmail.com>
2026-04-22 22:07:17 +0100

    Test final CI/CD

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
-app=Flask(__name__)
+
+app = Flask(__name__)
+
 @app.route("/")
 def home():
-     return "Version FINALE - CI/CD avec rollout restart !"
+    return "🚀 CI/CD Complet - Déployé automatiquement via Jenkins + GitHub + Kubernetes !"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
