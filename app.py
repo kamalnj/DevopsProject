9a38f35649f26541285e984ba216209d754e6a51
Kamal Ennaji <najikamal100@gmail.com>
2026-04-22 21:36:54 +0100

    last

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app=Flask(__name__)
 @app.route("/")
 def home():
-    return"Version 2 - Déployée automatiquement avec Jenkins et ngrok!"
+    return"lastversion"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
