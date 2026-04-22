7719ae75f27773c0427bf5c14b5a4505e636c1ab
Kamal Ennaji <najikamal100@gmail.com>
2026-04-22 01:47:08 +0100

    test3

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app=Flask(__name__)
 @app.route("/")
 def home():
-    return"Version 3 - Déployée automatiquement"
+    return"Version 4 - Déployée automatiquement"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
